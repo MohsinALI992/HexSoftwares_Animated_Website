96eeff8ce6eb9e9de1c84a6d7c5c2a90e3fc6904
Mohsin Ali <mohsintree123@gmail.com>
2026-03-27 09:39:50 +0500

    javascript file is updated

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Website Loaded Successfully");
+// simple scroll animation trigger (optional improvement)
+window.addEventListener("scroll", () => {
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        const position = card.getBoundingClientRect().top;
+        const screenHeight = window.innerHeight;
+
+        if(position < screenHeight - 50){
+            card.style.opacity = "1";
+        }
+    });
+});
